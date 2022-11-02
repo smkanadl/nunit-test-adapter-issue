455072f6d234867e9536dcfa10bfaebdaeba3985
Kai Nadler <kai.nadler@leybold.com>
2022-11-02 11:33:02 +0100

    initial commit

---
+++ src/Common/Sum.cs
@@ -0,0 +1,19 @@
+﻿namespace Common
+{
+    /// <summary>
+    /// Yeah
+    /// </summary>
+    public static class Sum
+    {
+        /// <summary>
+        ///  Comment
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double DoIt(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ src/Tests/Common.UnitTests/Sum.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Common
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void DoIt_2And3_Is5()
+        {
+            Assert.That(Sum.DoIt(2, 3), Is.EqualTo(5));
+        }
+    }
+}
